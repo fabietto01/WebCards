487886a892eedd34da5f2a20183ce8501bb7767e
fabietto01 <fabio.zorzetto.01@gmail.com>
2021-11-27 22:15:19 +0100

    fine controlli

--- WebCards/Models/MyGiocatori.cs
+++ WebCards/Models/MyGiocatori.cs
@@ -19,6 +19,7 @@ namespace WebCards.Models
                 CartaId = carta.Rowguid,
             };
             context.Manos.Add(mano);
+            context.SaveChanges();
         }
         
         public void Ruba(Giocatori giocatoreRubato, WebCarteContext context)
@@ -39,6 +40,7 @@ namespace WebCards.Models
 
         public void SpostaCarta(Carte carta, WebCarteContext context)
         {
+            //sposata la carta dalla mano al mazzo personale
             var mazzo = new MazzoPersonale
             {
                 CartaId = carta.Rowguid,
@@ -64,11 +66,7 @@ namespace WebCards.Models
                 ParitaId = PartiatId,
             };
             context.InTavolos.Add(intavolo);
-            var mano = new Mano
-            {
-                CartaId = carta.Rowguid,
-                GiocatoreId = Rowguid
-            };
+            var mano = context.Manos.FirstOrDefault(m => m.CartaId == carta.Rowguid && m.GiocatoreId == Rowguid);
             context.Manos.Remove(mano);
         }
 
