cce50dd317574fa606d07c5df8737620a1d0f04d
fabietto01 <fabio.zorzetto.01@gmail.com>
2021-11-29 21:58:36 +0100

    comento

--- WebCards/Startup.cs
+++ WebCards/Startup.cs
@@ -25,6 +25,7 @@ namespace WebCards
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //comento
             services.AddControllersWithViews();
             //services.Add(new ServiceDescriptor(typeof(WebCarteContext), new WebCarteContext()));
             services.AddDbContext<WebCarteContext>(options =>
