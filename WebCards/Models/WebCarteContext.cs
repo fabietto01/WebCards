d7f9d94c615a6c2613ec72f1be4062b3662f5775
fabio zorzetto <39068305+fabietto01@users.noreply.github.com>
2022-01-11 19:47:29 +0100

    Update WebCarteContext.cs

--- WebCards/Models/WebCarteContext.cs
+++ WebCards/Models/WebCarteContext.cs
@@ -31,7 +31,7 @@ namespace WebCards.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=185.25.206.129; MultipleActiveResultSets=true; Persist Security Info=True;User ID=ciro;Password=Ciro2021");
+                optionsBuilder.UseSqlServer("***********************************************************************************************");
             }
         }
 
